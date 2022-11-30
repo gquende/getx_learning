6177cffe84d22e131bbe48ed12b1ef2919db56c7
gquende <gquende@hotmail.com>
2022-11-30 16:12:34 +0100

    Init GetC

---
+++ android/app/src/main/kotlin/com/vipas/getx_learning/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vipas.getx_learning
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:getx_learning/home.dart';
+import 'package:get/get.dart';
+
+void main() async {
+  runApp(
+    MeuAplicativo(),
+  );
+}
+
+class MeuAplicativo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Brasileirão',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primarySwatch: Colors.grey,
+        accentColor: Colors.white,
+        accentIconTheme: IconThemeData(color: Colors.black),
+        dividerColor: Colors.black45,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            primary: Colors.deepPurpleAccent[100],
+          ),
+        ),
+      ),
+      themeMode: ThemeMode.system,
+      home: Home(),
+    );
+  }
+}
