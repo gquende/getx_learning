d5408f5f06f3024462219040c91776df74e7498b
gquende <gquende@hotmail.com>
2022-12-01 10:16:31 +0100

    A

--- lib/home.dart
+++ lib/home.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatefulWidget {
+import 'package:getx_learning/controller.dart';
+import 'package:get/get.dart';
+
+class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  @override
   Widget build(BuildContext context) {
+    final Controller c = Get.put(Controller());
+
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(
+        title: Obx(() => Text("Total de cliques ${c.count}")),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text("Ir para outra tela"),
+          onPressed: () => Get.to(Home()),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: c.increment,
+      ),
     );
   }
+}
+
+class Outra extends StatelessWidget {
+  // Você pode pedir o Get para encontrar o controller que foi usado em outra página e redirecionar você pra ele.
+  final Controller c = Get.find();
+  @override
+  Widget build(context) => Scaffold(body: Center(child: Text("${c.count}")));
 }
